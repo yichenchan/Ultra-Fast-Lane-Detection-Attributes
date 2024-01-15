bb26f3d4a77a54b0afbb0c371d7b34e6ee8adcbc
eachan.chen <c840098794@gmail.com>
2024-01-15 16:13:18 +0800

    修改训练测试代码

--- test.py
+++ test.py
@@ -4,30 +4,169 @@ from utils.common import merge_config
 from utils.dist_utils import dist_print
 from evaluation.eval_wrapper import eval_lane
 import torch
+from data.dataloader import get_dataloader
+from utils.dist_utils import dist_print, dist_tqdm, is_main_process, DistSummaryWriter
+import scipy
+
+eps = 0.0000001
+
+def test(net, test_loader, cfg, local_rank):
+    net.eval()
+
+    x_pos_correct_total = [0] * 12
+    x_pos_total = [0] * 12
+    x_pos_tp_total = [0] * 12
+    x_pos_p_total = [0] * 12
+    color_correct_total = [0] * 12
+    color_total = [0] * 12
+    color_tp_total = [0] * 12
+    color_p_total = [0] * 12
+    type_correct_total = [0] * 12
+    type_total = [0] * 12
+    type_tp_total = [0] * 12
+    type_p_total = [0] * 12
+    func_correct_total = [0] * 12
+    func_total = [0] * 12
+    func_tp_total = [0] * 12
+    func_p_total = [0] * 12
+    img_num_total = 0
+    status_correct_total = 0
+    status_tp_total = 0
+    status_p_total = 0
+    lighting_correct_total = 0
+    lighting_tp_total = 0
+    lighting_p_total = 0
+    weather_correct_total = 0
+    weather_tp_total = 0
+    weather_p_total = 0    
+    road_construction_correct_total = 0
+    road_construction_tp_total = 0
+    road_construction_p_total = 0
+    for i, data in enumerate(dist_tqdm(test_loader)):
+        # 获取标签
+        img, status_label, lighting_label, weather_label, road_construction_label, cls_label, type_label, func_label, color_label = data
+        img, status_label, lighting_label, weather_label, road_construction_label, cls_label_, type_label_, func_label_, color_label_ = \
+        img.cuda(), status_label.cuda(), lighting_label.cuda(), weather_label.cuda(), road_construction_label.cuda(), cls_label.cuda(), type_label.cuda(), func_label.cuda(), color_label.cuda()
+
+        # 获取预测输出
+        cls_out, color_out, func_out, type_out, status_out, lighting_out, weather_out, env_out = net(img)
+        
+        # 获取状态输出
+        status_out_prob = torch.nn.functional.softmax(status_out, dim=1)
+        status_out_max_index = torch.argmax(status_out_prob, dim=1)
+        status_correct_total += (status_out_max_index == status_label).sum().item()
+        status_tp_total += ((status_out_max_index == status_label) & (status_label == 0)).sum().item()
+        status_p_total += (status_label == 0).sum().item()
+
+        # 获取时间输出
+        lighting_out_prob = torch.nn.functional.softmax(lighting_out, dim=1)
+        lighting_out_max_index = torch.argmax(lighting_out_prob, dim=1)
+        lighting_correct_total += (lighting_out_max_index == lighting_label).sum().item()
+        lighting_tp_total += ((lighting_out_max_index == lighting_label) & (lighting_label != 2)).sum().item()
+        lighting_p_total += (lighting_label != 2).sum().item()
+
+        # 获取天气输出
+        weather_out_prob = torch.nn.functional.softmax(weather_out, dim=1)
+        weather_out_max_index = torch.argmax(weather_out_prob, dim=1)
+        weather_correct_total += (weather_out_max_index == weather_label).sum().item()
+        weather_tp_total += ((weather_out_max_index == weather_label) & (weather_label != 4)).sum().item()
+        weather_p_total += (weather_label != 4).sum().item()
+
+        # 获取道路情况输出
+        env_out_prob = torch.nn.functional.softmax(env_out, dim=1)
+        env_out_max_index = torch.argmax(env_out_prob, dim=1)
+        road_construction_correct_total += (env_out_max_index == road_construction_label).sum().item()
+        road_construction_tp_total += ((env_out_max_index == road_construction_label) & (road_construction_label != 4)).sum().item()
+        road_construction_p_total += (road_construction_label != 4).sum().item()
+
+        # 过滤所有正常图片的 mask(非正常状态不进行测试)
+        normal_status_mask = (status_label == 0)
+        img_num_total += img.shape[0]
+
+        # 获取道路线 x 坐标输出
+        cls_out_prob = torch.nn.functional.softmax(cls_out, dim=1)
+        cls_out_max_index_ = torch.argmax(cls_out_prob, dim=1)
+
+        # 获取道路线颜色输出
+        color_out_prob = torch.nn.functional.softmax(color_out, dim=1)
+        color_out_max_index_ = torch.argmax(color_out_prob, dim=1)
+
+        # 获取道路线类型输出
+        type_out_prob = torch.nn.functional.softmax(type_out, dim=1)
+        type_out_max_index_ = torch.argmax(type_out_prob, dim=1)
+
+        # 获取道路线功能输出
+        func_out_prob = torch.nn.functional.softmax(func_out, dim=1)
+        func_out_max_index_ = torch.argmax(func_out_prob, dim=1)
+
+        for i in range(12):
+            cls_label = cls_label_[normal_status_mask] # 筛选 normal 的样例
+            cls_out_max_index = cls_out_max_index_[normal_status_mask] # 筛选 normal 的样例
+            x_pos_correct_total[i] += (cls_out_max_index[:, :, i] == cls_label[:, :, i]).sum().item()
+            x_pos_total[i] += cls_label[:, :, i].numel()
+            x_pos_tp_total[i] += ((cls_out_max_index[:, :, i] == cls_label[:, :, i]) & (cls_label[:, :, i] != cfg.num_x_grid)).sum().item()
+            x_pos_p_total[i] += (cls_label[:, :, i] != cfg.num_x_grid).sum().item()
+            
+            color_label = color_label_[normal_status_mask] # 筛选 normal 的样例
+            color_out_max_index = color_out_max_index_[normal_status_mask] # 筛选 normal 的样例
+            color_correct_total[i] += (color_out_max_index[:, :, i] == color_label[:, :, i]).sum().item()
+            color_total[i] += color_label[:, :, i].numel()
+            color_tp_total[i] += ((color_out_max_index[:, :, i] == color_label[:, :, i]) & (color_label[:, :, i] != 0)).sum().item()
+            color_p_total[i] += (color_label[:, :, i] != 0).sum().item()
+            
+            type_label = type_label_[normal_status_mask] # 筛选 normal 的样例
+            type_out_max_index = type_out_max_index_[normal_status_mask] # 筛选 normal 的样例
+            type_correct_total[i] += (type_out_max_index[:, :, i] == type_label[:, :, i]).sum().item()
+            type_total[i] += type_label[:, :, i].numel()
+            type_tp_total[i] += ((type_out_max_index[:, :, i] == type_label[:, :, i]) & (type_label[:, :, i] != 0)).sum().item()
+            type_p_total[i] += (type_label[:, :, i] != 0).sum().item()
+
+            func_label = func_label_[normal_status_mask] # 筛选 normal 的样例
+            func_out_max_index = func_out_max_index_[normal_status_mask] # 筛选 normal 的样例
+            func_correct_total[i] += (func_out_max_index[:, :, i] == func_label[:, :, i]).sum().item()
+            func_total[i] += func_label[:, :, i].numel()
+            func_tp_total[i] += ((func_out_max_index[:, :, i] == func_label[:, :, i]) & (func_label[:, :, i] != 0)).sum().item()
+            func_p_total[i] += (func_label[:, :, i] != 0).sum().item()                        
+
+    if is_main_process():
+        print("             precision(total)", "      recall(positive_total)")
+        print("status         ", format(status_correct_total / (img_num_total + eps), ".4f"), "(", img_num_total, ")           ", \
+                                format(status_tp_total / (status_p_total + eps), ".4f"), "(", status_p_total, ")")
+        print("lighting       ", format(lighting_correct_total / (img_num_total + eps), ".4f"), "(", img_num_total, ")           ", \
+                                format(lighting_tp_total / (lighting_p_total + eps), ".4f"), "(", lighting_p_total, ")")
+        print("weather        ", format(weather_correct_total / (img_num_total + eps), ".4f"), "(", img_num_total, ")           ", \
+                                format(weather_tp_total / (weather_p_total + eps), ".4f"), "(", weather_p_total, ")")
+        print("urbanOrNot     ", format(road_construction_correct_total / (img_num_total + eps), ".4f"), "(", img_num_total, ")           ", \
+                                format(road_construction_tp_total / (road_construction_p_total + eps), ".4f"), "(", road_construction_p_total, ")")            
+
+        for i in range(12):
+            print("line", format(i, "02d"), "x      ", format(x_pos_correct_total[i] / (x_pos_total[i] + eps), ".4f"), "(", x_pos_total[i], ")          ", \
+                                        format(x_pos_tp_total[i] / (x_pos_p_total[i] + eps), ".4f"), "(", x_pos_p_total[i], ")")
+            print("line", format(i, "02d"), "color  ", format(color_correct_total[i] / (color_total[i] + eps), ".4f"), "(", color_total[i], ")          ", \
+                                        format(color_tp_total[i] / (color_p_total[i] + eps), ".4f"), "(", color_p_total[i], ")")
+            print("line", format(i, "02d"), "type   ", format(type_correct_total[i] / (type_total[i] + eps), ".4f"), "(", type_total[i], ")          ", \
+                                        format(type_tp_total[i] / (type_p_total[i] + eps), ".4f"), "(", type_p_total[i], ")")
+            print("line", format(i, "02d"), "func   ", format(func_correct_total[i] / (func_total[i] + eps), ".4f"), "(", func_total[i], ")          ", \
+                                        format(func_tp_total[i] / (func_p_total[i] + eps), ".4f"), "(", func_p_total[i], ")")                                    
+
+
 if __name__ == "__main__":
     torch.backends.cudnn.benchmark = True
 
     args, cfg = merge_config()
 
     distributed = False
-    if 'WORLD_SIZE' in os.environ:
-        distributed = int(os.environ['WORLD_SIZE']) > 1
+    # if 'WORLD_SIZE' in os.environ:
+    #     distributed = int(os.environ['WORLD_SIZE']) > 1
 
-    if distributed:
-        torch.cuda.set_device(args.local_rank)
-        torch.distributed.init_process_group(backend='nccl', init_method='env://')
+    # if distributed:
+    #     torch.cuda.set_device(args.local_rank)
+    #     torch.distributed.init_process_group(backend='nccl', init_method='env://')
+
     dist_print('start testing...')
     assert cfg.backbone in ['18','34','50','101','152','50next','101next','50wide','101wide']
 
-    if cfg.dataset == 'CULane':
-        cls_num_per_lane = 18
-    elif cfg.dataset == 'Tusimple':
-        cls_num_per_lane = 56
-    else:
-        raise NotImplementedError
-
-    net = parsingNet(pretrained = False, backbone=cfg.backbone,cls_dim = (cfg.griding_num+1,cls_num_per_lane, cfg.num_lanes),
-                    use_aux=False).cuda() # we dont need auxiliary segmentation in testing
+    net = parsingNet(pretrained = False, backbone=cfg.backbone, objectness_dim = (cfg.num_x_grid+1, cfg.num_y_grid, cfg.num_lanes), use_aux=False).cuda() 
 
     state_dict = torch.load(cfg.test_model, map_location = 'cpu')['model']
     compatible_state_dict = {}
@@ -42,7 +181,6 @@ if __name__ == "__main__":
     if distributed:
         net = torch.nn.parallel.DistributedDataParallel(net, device_ids = [args.local_rank])
 
-    if not os.path.exists(cfg.test_work_dir):
-        os.mkdir(cfg.test_work_dir)
+    test_loader = get_dataloader(cfg.test_batch_size, cfg.data_root, cfg.num_x_grid, cfg.test_dataset, False, False, distributed, cfg.num_lanes)
 
-    eval_lane(net, cfg.dataset, cfg.data_root, cfg.test_work_dir, cfg.griding_num, False, distributed)
+    test(net, test_loader, cfg)
--- train.py
+++ train.py
@@ -1,25 +1,45 @@
 import torch, os, datetime
 import numpy as np
-
+# import torch
+import torch.nn as nn
 from model.model import parsingNet
-from data.dataloader import get_train_loader
+from data.dataloader import get_dataloader
 
 from utils.dist_utils import dist_print, dist_tqdm, is_main_process, DistSummaryWriter
 from utils.factory import get_metric_dict, get_loss_dict, get_optimizer, get_scheduler
 from utils.metrics import MultiLabelAcc, AccTopk, Metric_mIoU, update_metrics, reset_metrics
 
+from utils.loss import SoftmaxFocalLoss, ParsingRelationLoss, ParsingRelationDis
+from utils.metrics import MultiLabelAcc, AccTopk, Metric_mIoU
+from utils.dist_utils import DistSummaryWriter
+
 from utils.common import merge_config, save_model, cp_projects
 from utils.common import get_work_dir, get_logger
 
+from evaluation.eval_wrapper import eval_lane
+from test import test
 import time
 
 
 def inference(net, data_label, use_aux):
+    
     if use_aux:
-        img, cls_label, seg_label = data_label
-        img, cls_label, seg_label = img.cuda(), cls_label.long().cuda(), seg_label.long().cuda()
-        cls_out, seg_out = net(img)
-        return {'cls_out': cls_out, 'cls_label': cls_label, 'seg_out':seg_out, 'seg_label': seg_label}
+        img, status_label, lighting_label, weather_label, road_construction_label, cls_label, pts_type, pts_func, pts_color, seg_label = data_label
+        img, status_label, lighting_label, weather_label, road_construction_label, cls_label, pts_type, pts_func, pts_color, seg_label = \
+        img.cuda(), status_label.cuda(), lighting_label.cuda(), weather_label.cuda(), road_construction_label.cuda(), cls_label.cuda(), pts_type.cuda(), pts_func.cuda(), pts_color.cuda(), seg_label.cuda()
+        cls_out, color_out, func_out, type_out, status_out, lighting_out, weather_out, env_out, seg_out = net(img)
+        
+        return {'cls_out': cls_out, 'cls_label': cls_label, 
+                'status_out':status_out, 'status_label': status_label,
+                'lighting_out':lighting_out, 'lighting_label': lighting_label,
+                'weather_out':weather_out, 'weather_label': weather_label,
+                'env_out':env_out, 'env_label': road_construction_label,
+                'type_out':type_out, 'type_label': pts_type,
+                'func_out':func_out, 'func_label': pts_func,
+                'color_out':color_out, 'color_label': pts_color,
+                'seg_out':seg_out, 'seg_label': seg_label   
+        }
+
     else:
         img, cls_label = data_label
         img, cls_label = img.cuda(), cls_label.long().cuda()
@@ -37,43 +57,53 @@ def resolve_val_data(results, use_aux):
 def calc_loss(loss_dict, results, logger, global_step):
     loss = 0
 
-    for i in range(len(loss_dict['name'])):
+    loss_status = nn.CrossEntropyLoss()(results['status_out'], results['status_label'])
+    loss_lighting = nn.CrossEntropyLoss()(results['lighting_out'], results['lighting_label'])
+    loss_weather = nn.CrossEntropyLoss()(results['weather_out'], results['weather_label'])
+    loss_env = nn.CrossEntropyLoss()(results['env_out'], results['env_label'])
 
-        data_src = loss_dict['data_src'][i]
+    cls_label = results['cls_label'][results['status_label'] == 0, :, :]
+    cls_out = results['cls_out'][results['status_label'] == 0, :, :, :] # [1, 201, 18, 12]
+    loss_cls = SoftmaxFocalLoss(2)(cls_out, cls_label) + 0.0 * ParsingRelationLoss()(cls_out) + 0.0 * ParsingRelationDis()(cls_out)
 
-        datas = [results[src] for src in data_src]
+    pts_type = results['type_label'][results['status_label']==0, :, :]
+    type_out = results['type_out'][results['status_label']==0, :, :, :]
+    loss_type = SoftmaxFocalLoss(2)(type_out, pts_type.long())
 
-        loss_cur = loss_dict['op'][i](*datas)
+    pts_func = results['func_label'][results['status_label']==0, :, :]
+    func_out = results['func_out'][results['status_label']==0, :, :, :]
+    loss_func = SoftmaxFocalLoss(2)(func_out, pts_func.long())
 
-        if global_step % 20 == 0:
-            logger.add_scalar('loss/'+loss_dict['name'][i], loss_cur, global_step)
+    pts_color = results['color_label'][results['status_label']==0, :, :]
+    color_out = results['color_out'][results['status_label']==0, :, :, :]
+    loss_color = SoftmaxFocalLoss(2)(color_out, pts_color.long())
 
-        loss += loss_cur * loss_dict['weight'][i]
-    return loss
+    seg_label = results['seg_label'][results['status_label']==0, :, :]
+    seg_out = results['seg_out'][results['status_label']==0, :, :, :]
+    loss_seg = torch.nn.CrossEntropyLoss()(seg_out, seg_label.long())
+
+    loss = (loss_status + loss_lighting + loss_weather + loss_env) * 0.01 + loss_cls  + loss_type + loss_func + loss_color + loss_seg 
+    return loss, (loss_status, loss_lighting, loss_weather, loss_env, loss_cls, loss_type, loss_func, loss_color, loss_seg)
 
 
-def train(net, data_loader, loss_dict, optimizer, scheduler,logger, epoch, metric_dict, use_aux):
-    net.train()
-    progress_bar = dist_tqdm(train_loader)
-    t_data_0 = time.time()
+def train(net, train_dataloader, loss_dict, optimizer, scheduler,logger, epoch, metric_dict, use_aux):
+    progress_bar = dist_tqdm(train_dataloader)
+
     for b_idx, data_label in enumerate(progress_bar):
-        t_data_1 = time.time()
+        global_step = epoch * len(train_dataloader) + b_idx
+
+        # 训练
+        net.train()
         reset_metrics(metric_dict)
-        global_step = epoch * len(data_loader) + b_idx
-
-        t_net_0 = time.time()
         results = inference(net, data_label, use_aux)
-
-        loss = calc_loss(loss_dict, results, logger, global_step)
+        loss, sep_loss = calc_loss(loss_dict, results, logger, global_step)
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
         scheduler.step(global_step)
-        t_net_1 = time.time()
-
         results = resolve_val_data(results, use_aux)
-
         update_metrics(metric_dict, results)
+
         if global_step % 20 == 0:
             for me_name, me_op in zip(metric_dict['name'], metric_dict['op']):
                 logger.add_scalar('metric/' + me_name, me_op.get(), global_step=global_step)
@@ -81,18 +111,21 @@ def train(net, data_loader, loss_dict, optimizer, scheduler,logger, epoch, metri
 
         if hasattr(progress_bar,'set_postfix'):
             kwargs = {me_name: '%.3f' % me_op.get() for me_name, me_op in zip(metric_dict['name'], metric_dict['op'])}
-            progress_bar.set_postfix(loss = '%.3f' % float(loss), 
-                                    data_time = '%.3f' % float(t_data_1 - t_data_0), 
-                                    net_time = '%.3f' % float(t_net_1 - t_net_0), 
+            progress_bar.set_postfix(total = '%.3f' % float(loss),        
+                                    status = '%.3f' % sep_loss[0],
+                                    light = '%.3f' % sep_loss[1],
+                                    weather = '%.3f' % sep_loss[2],
+                                    env = '%.3f' % sep_loss[3],
+                                    x = '%.3f' % sep_loss[4],
+                                    type = '%.3f' % sep_loss[5],
+                                    func = '%.3f' % sep_loss[6],
+                                    color = '%.3f' % sep_loss[7],
                                     **kwargs)
+
         t_data_0 = time.time()
         
-
-
-
-
-
 if __name__ == "__main__":
+
     torch.backends.cudnn.benchmark = True
 
     args, cfg = merge_config()
@@ -102,22 +135,22 @@ if __name__ == "__main__":
     distributed = False
     if 'WORLD_SIZE' in os.environ:
         distributed = int(os.environ['WORLD_SIZE']) > 1
-
     if distributed:
         torch.cuda.set_device(args.local_rank)
         torch.distributed.init_process_group(backend='nccl', init_method='env://')
+
     dist_print(datetime.datetime.now().strftime('[%Y/%m/%d %H:%M:%S]') + ' start training...')
     dist_print(cfg)
     assert cfg.backbone in ['18','34','50','101','152','50next','101next','50wide','101wide']
 
+    # 创建训练数据集
+    train_loader = get_dataloader(cfg.train_batch_size, cfg.data_root, cfg.num_x_grid, cfg.train_dataset, cfg.use_augment_in_train, cfg.use_aux, distributed, cfg.num_lanes, cfg.resized_width, cfg.resized_height)
 
-    train_loader, cls_num_per_lane = get_train_loader(cfg.batch_size, cfg.data_root, cfg.griding_num, cfg.dataset, cfg.use_aux, distributed, cfg.num_lanes)
+    # 只在第一块 gpu 上进行验证,创建验证集
+    train_loader_for_val = get_dataloader(cfg.val_batch_size, cfg.data_root, cfg.num_x_grid, cfg.train_dataset, cfg.use_augment_in_val, False, False, cfg.num_lanes, cfg.resized_width, cfg.resized_height)
+    val_loader = get_dataloader(cfg.val_batch_size, cfg.data_root, cfg.num_x_grid, cfg.val_dataset, cfg.use_augment_in_val, False, False, cfg.num_lanes, cfg.resized_width, cfg.resized_height)
 
-    net = parsingNet(pretrained = True, backbone=cfg.backbone,cls_dim = (cfg.griding_num+1,cls_num_per_lane, cfg.num_lanes),use_aux=cfg.use_aux).cuda()
-
-    if distributed:
-        net = torch.nn.parallel.DistributedDataParallel(net, device_ids = [args.local_rank])
-    optimizer = get_optimizer(net, cfg)
+    net = parsingNet(pretrained = True, backbone=cfg.backbone, objectness_dim = (cfg.num_x_grid + 1, cfg.num_y_grid, cfg.num_lanes), use_aux=cfg.use_aux).cuda()
 
     if cfg.finetune is not None:
         dist_print('finetune from ', cfg.finetune)
@@ -127,17 +160,28 @@ if __name__ == "__main__":
             if 'model' in k:
                 state_clip[k] = v
         net.load_state_dict(state_clip, strict=False)
+        # 冻结编码器部分
+        # for name, param in net.named_parameters():
+        #     if 'model' in name:
+        #         param.requires_grad = False
+
+    if distributed:
+        dummy_input = torch.randn(1, 3, cfg.resized_height, cfg.resized_width).cuda()
+        net(dummy_input)
+        net = torch.nn.parallel.DistributedDataParallel(net, device_ids = [args.local_rank])
+
+    optimizer = get_optimizer(net, cfg)
+
     if cfg.resume is not None:
         dist_print('==> Resume model from ' + cfg.resume)
         resume_dict = torch.load(cfg.resume, map_location='cpu')
-        net.load_state_dict(resume_dict['model'])
+        net.load_state_dict(resume_dict['model'], strict=False)
         if 'optimizer' in resume_dict.keys():
             optimizer.load_state_dict(resume_dict['optimizer'])
         resume_epoch = int(os.path.split(cfg.resume)[1][2:5]) + 1
+        work_dir = os.path.dirname(cfg.resume)
     else:
         resume_epoch = 0
-
-
 
     scheduler = get_scheduler(optimizer, cfg, len(train_loader))
     dist_print(len(train_loader))
@@ -147,8 +191,18 @@ if __name__ == "__main__":
     cp_projects(args.auto_backup, work_dir)
 
     for epoch in range(resume_epoch, cfg.epoch):
-
-        train(net, train_loader, loss_dict, optimizer, scheduler,logger, epoch, metric_dict, cfg.use_aux)
+        # 训练
+        train(net, train_loader, loss_dict, optimizer, scheduler, logger, epoch, metric_dict, cfg.use_aux)
         
-        save_model(net, optimizer, epoch ,work_dir, distributed)
+        # 验证
+        # test(net, train_loader_for_val, cfg, args.local_rank)
+        test(net, val_loader, cfg, args.local_rank)
+
+        # 保存
+        save_model(net, optimizer, epoch , work_dir, distributed)
+
+    # 进行最终测试
+    test_loader = get_dataloader(cfg.test_batch_size, cfg.data_root, cfg.num_x_grid, cfg.test_dataset, cfg.use_augment_in_test, False, distributed, cfg.num_lanes, cfg.resized_width, cfg.resized_height)
+    test(net, test_loader, cfg, args.local_rank)
+
     logger.close()
